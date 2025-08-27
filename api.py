05cebf202cdb2cf1a3fb5a34b89e59ca65718a6d
Steven-Ou <osteve425@gmail.com>
2025-08-27 18:54:05 -0400

    adding in request to React app and also the initializing the FRED service

--- api.py
+++ api.py
@@ -6,3 +6,12 @@ import FRED_data_service
 import property_math
 
 app = Flask(__name__)
+CORS(app,resources = {r"/api/*":{"origins":"http://localhost:3000"}})
+
+try:
+    fred_data_service = FRED_data_service.FRED_data(API_key=os.getenv("FRED_API", ""))
+except Exception as e:
+    print(f"Could not initialize FRED service:{e}")
+    fred_data_service = None
+    
+
