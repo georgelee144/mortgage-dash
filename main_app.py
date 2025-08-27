ff3cbcacd96e5d203a3f9536797236b4221fd39c
Steven-Ou <osteve425@gmail.com>
2025-08-27 18:03:36 -0400

    adding in external css and server = app.server for deployment use

--- main_app.py
+++ main_app.py
@@ -5,3 +5,8 @@ import plotly.graph_objects as go
 import pandas as pd
 import FRED_data_service
 import property_math
+
+
+external_css = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_css)
+server = app.server #for deployment use
