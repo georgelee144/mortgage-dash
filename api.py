38292b14bbc72d14bd247791b5a63c476016cfda
Steven-Ou <osteve425@gmail.com>
2025-08-27 18:59:35 -0400

    creating the get Current rate function and the app route to link to the react

--- api.py
+++ api.py
@@ -14,4 +14,15 @@ except Exception as e:
     print(f"Could not initialize FRED service:{e}")
     fred_data_service = None
     
+    
+@app.route("/api/current-rate",methods=['GET'])
+def get_current_rate():
+    if not fred_data_service:
+        return jsonify({"error": "FRED service not available"}),500
+    try:
+        rate = fred_data_service.get_most_recent_interest_rate()
+        return jsonify({"rate":float(rate)})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 
