121ab26c643ae840b99048b2fa2bebd8435fc31d
Steven-Ou <osteve425@gmail.com>
2025-08-27 22:16:27 -0400

    Input states

--- mortgage-frontend/app/page.js
+++ mortgage-frontend/app/page.js
@@ -7,4 +7,6 @@ export default function Home() {
   const [activeTab, setActiveTab] = useState("calculator");
   const [loanAmount, setLoanAmount] = useState(500000);
   const [propertyValue, setPropertyValue] = useState(600000);
+    const [annualRate, setAnnualRate] = useState('');
+
 }
