feb1b15b05fc0ad3415ecd2292601480c1a3bce8
Steven-Ou <osteve425@gmail.com>
2025-08-27 22:16:46 -0400

    Input states

--- mortgage-frontend/app/page.js
+++ mortgage-frontend/app/page.js
@@ -7,7 +7,9 @@ export default function Home() {
   const [activeTab, setActiveTab] = useState("calculator");
   const [loanAmount, setLoanAmount] = useState(500000);
   const [propertyValue, setPropertyValue] = useState(600000);
-    const [annualRate, setAnnualRate] = useState('');
+  const [annualRate, setAnnualRate] = useState("");
   const [termInMonths, setTermInMonths] = useState(360);
-
+  const [priceIndexKey, setPriceIndexKey] = useState(
+    "S&P CoreLogic Case-Shiller U.S. National Home Price Index"
+  );
 }
