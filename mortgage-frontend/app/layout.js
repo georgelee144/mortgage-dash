8c80db7ae7f647329c67278092bc756675251d47
Steven-Ou <osteve425@gmail.com>
2025-08-31 01:59:49 -0400

    fixing

--- mortgage-frontend/app/layout.js
+++ mortgage-frontend/app/layout.js
@@ -1,18 +1,14 @@
-import { Inter } from "next/font/google";
-import "./globals.css"; // Import the global stylesheet
-
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata = {
-  title: "Home Buyer's Financial Dashboard",
-  description:
-    "Calculate mortgage payments and simulate future property values.",
+  title: "Mortgage Dashboard",
+  description: "Financial tools for home buyers",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
