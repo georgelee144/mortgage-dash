b82d879a7e9a03a378aa5ce9a4f9f8fefcbb0a28
Steven-Ou <osteve425@gmail.com>
2025-09-05 12:57:36 -0400

    removing BlockMath and the katex thingy

--- mortgage-frontend/app/page.js
+++ mortgage-frontend/app/page.js
@@ -2,8 +2,6 @@
 
 import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
-import "katex/dist/katex.min.css";
-import { BlockMath } from "react-katex";
 
 // --- Dynamically import the Plot component with SSR turned off ---
 const Plot = dynamic(() => import("react-plotly.js"), {
@@ -413,7 +411,13 @@ export default function Home() {
                 Mortgage payment is calculated, by solving for the payment
                 variable of the present value annuity formula.
               </p>
-              <BlockMath math="\\text{present value of annuity} = \\text{payment} \\times \\frac{1 - (1 + \\frac{i}{n})^{-mn}}{\\frac{i}{n}}" />
+              <div className="formula">
+                <code>
+                  present_value_of_annuity = payment × [ (1 - (1 + i/n)
+                  <sup>-mn</sup>) / (i/n) ]
+                </code>
+              </div>
+
               <ul className="formula-vars">
                 <li>
                   <strong>payment</strong> is a constant amount we pay at the
@@ -435,9 +439,19 @@ export default function Home() {
                 We can substitute present_value_of_annuity for loan_amount, i
                 for the APR and n for 12.
               </p>
-              <BlockMath math="\\text{loan amount} = \\text{payment} \\times \\frac{1 - (1 + \\frac{APR}{12})^{-12m}}{\\frac{APR}{12}}" />
+              <div className="formula">
+                <code>
+                  loan_amount = payment × [ (1 - (1 + APR/12)<sup>-12m</sup>) /
+                  (APR/12) ]
+                </code>
+              </div>
               <p>Solving for payment we get:</p>
-              <BlockMath math="\\text{payment} = \\text{loan amount} \\times \\frac{\\frac{APR}{12}}{1 - (1 + \\frac{APR}{12})^{-12m}}" />
+              <div className="formula">
+                <code>
+                  payment = loan_amount × [ (APR/12) / (1 - (1 + APR/12)
+                  <sup>-12m</sup>) ]
+                </code>
+              </div>
               <p>
                 If extra payments were added, we assumed that it was done at the
                 end of the month.
