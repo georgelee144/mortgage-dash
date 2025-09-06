7e79a39f19fb36ebbb314845a06dc4a4e06a3955
George Lee <george@ml.cafe>
2025-09-06 13:07:53 -0400

    adding back function that lanuchs flask api app

--- api.py
+++ api.py
@@ -144,3 +144,8 @@ def get_mortgage_options():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
